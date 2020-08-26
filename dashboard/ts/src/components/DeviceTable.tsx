5b5770d629392388b27af6ca399873b799868258
CallumHoward <callumh@cse.unsw.edu.au>
2020-08-27 01:22:37 +1000

    tidy up

--- dashboard/ts/src/components/DeviceTable.tsx
+++ dashboard/ts/src/components/DeviceTable.tsx
@@ -61,39 +61,6 @@ const DeviceTable: React.FunctionComponent<Props> = ({ data }) => {
     []
   );
 
-  //const data = React.useMemo(
-  //  () => [
-  //    {
-  //      col1: "",
-  //      col2: "Stacey",
-  //      col3: "",
-  //      col4: "",
-  //      col5: "",
-  //      col6: "",
-  //      col7: "",
-  //    },
-  //    {
-  //      col1: "",
-  //      col2: "Baby",
-  //      col3: "",
-  //      col4: "",
-  //      col5: "",
-  //      col6: "",
-  //      col7: "",
-  //    },
-  //    {
-  //      col1: "",
-  //      col2: "Mya",
-  //      col3: "",
-  //      col4: "",
-  //      col5: "",
-  //      col6: "",
-  //      col7: "",
-  //    },
-  //  ],
-  //  []
-  //);
-
   const {
     getTableProps,
     getTableBodyProps,
@@ -101,7 +68,6 @@ const DeviceTable: React.FunctionComponent<Props> = ({ data }) => {
     rows,
     toggleAllRowsSelected,
     prepareRow,
-    state,
   } = useTable(
     {
       autoResetSelectedRows: false,
@@ -113,60 +79,56 @@ const DeviceTable: React.FunctionComponent<Props> = ({ data }) => {
   );
 
   return (
-    <>
-      <Table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {column.render("Header")}
-                  <span style={{ paddingLeft: "0.5rem" }}>
-                    {column.isSorted ? (
-                      column.isSortedDesc ? (
-                        <FontAwesomeIcon icon={faAngleUp} />
-                      ) : (
-                        <FontAwesomeIcon icon={faAngleDown} />
-                      )
+    <Table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                {column.render("Header")}
+                <span style={{ paddingLeft: "0.5rem" }}>
+                  {column.isSorted ? (
+                    column.isSortedDesc ? (
+                      <FontAwesomeIcon icon={faAngleUp} />
                     ) : (
-                      <FontAwesomeIcon
-                        icon={faAngleDown}
-                        style={{ opacity: 0 }}
-                      />
-                    )}
-                  </span>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
+                      <FontAwesomeIcon icon={faAngleDown} />
+                    )
+                  ) : (
+                    <FontAwesomeIcon
+                      icon={faAngleDown}
+                      style={{ opacity: 0 }}
+                    />
+                  )}
+                </span>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
 
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <StyledTr
-                {...row.getRowProps({
-                  style: {
-                    backgroundColor: row.isSelected ? "#eee" : "",
-                  },
-                })}
-                onClick={(e: any) => {
-                  toggleAllRowsSelected(false);
-                  row.toggleRowSelected();
-                }}
-              >
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
-                })}
-              </StyledTr>
-            );
-          })}
-        </tbody>
-      </Table>
-    </>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <StyledTr
+              {...row.getRowProps({
+                style: {
+                  backgroundColor: row.isSelected ? "#eee" : "",
+                },
+              })}
+              onClick={(e: any) => {
+                toggleAllRowsSelected(false);
+                row.toggleRowSelected();
+              }}
+            >
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </StyledTr>
+          );
+        })}
+      </tbody>
+    </Table>
   );
 };
 
