d551d9cf142cd982198bac46358113476489e58b
CallumHoward <callumh@cse.unsw.edu.au>
2020-08-30 08:48:31 +1000

    update to use device proto and do gofmt

--- dashboard/go/exampleserver/exampleserver.go
+++ dashboard/go/exampleserver/exampleserver.go
@@ -1,114 +1,122 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "log"
-  "net/http"
-  "os"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-  "google.golang.org/grpc"
-  "google.golang.org/grpc/codes"
-  "google.golang.org/grpc/grpclog"
-  "google.golang.org/grpc/metadata"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/metadata"
 
-  "strings"
-
-  library "origin/dashboard/go/_proto/examplecom/library"
-  "github.com/improbable-eng/grpc-web/go/grpcweb"
-  "golang.org/x/net/context"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	"golang.org/x/net/context"
+	library "origin/dashboard/go/_proto/examplecom/library"
 )
 
 var (
-  enableTls       = flag.Bool("enable_tls", false, "Use TLS - required for HTTP2.")
-  tlsCertFilePath = flag.String("tls_cert_file", "../../misc/localhost.crt", "Path to the CRT/PEM file.")
-  tlsKeyFilePath  = flag.String("tls_key_file", "../../misc/localhost.key", "Path to the private key file.")
+	enableTls       = flag.Bool("enable_tls", false, "Use TLS - required for HTTP2.")
+	tlsCertFilePath = flag.String("tls_cert_file", "../../misc/localhost.crt", "Path to the CRT/PEM file.")
+	tlsKeyFilePath  = flag.String("tls_key_file", "../../misc/localhost.key", "Path to the private key file.")
 )
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  port := 9090
-  if *enableTls {
-    port = 9091
-  }
+	port := 9090
+	if *enableTls {
+		port = 9091
+	}
 
-  grpcServer := grpc.NewServer()
-  library.RegisterBookServiceServer(grpcServer, &bookService{})
-  grpclog.SetLogger(log.New(os.Stdout, "exampleserver: ", log.LstdFlags))
+	grpcServer := grpc.NewServer()
+	library.RegisterDeviceServiceServer(grpcServer, &deviceService{})
+	grpclog.SetLogger(log.New(os.Stdout, "exampleserver: ", log.LstdFlags))
 
-  wrappedServer := grpcweb.WrapServer(grpcServer)
-  handler := func(resp http.ResponseWriter, req *http.Request) {
-    wrappedServer.ServeHTTP(resp, req)
-  }
+	wrappedServer := grpcweb.WrapServer(grpcServer)
+	handler := func(resp http.ResponseWriter, req *http.Request) {
+		wrappedServer.ServeHTTP(resp, req)
+	}
 
-  httpServer := http.Server{
-    Addr:    fmt.Sprintf(":%d", port),
-    Handler: http.HandlerFunc(handler),
-  }
+	httpServer := http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: http.HandlerFunc(handler),
+	}
 
-  grpclog.Printf("Starting server. http port: %d, with TLS: %v", port, *enableTls)
+	grpclog.Printf("Starting server. http port: %d, with TLS: %v", port, *enableTls)
 
-  if *enableTls {
-    if err := httpServer.ListenAndServeTLS(*tlsCertFilePath, *tlsKeyFilePath); err != nil {
-      grpclog.Fatalf("failed starting http2 server: %v", err)
-    }
-  } else {
-    if err := httpServer.ListenAndServe(); err != nil {
-      grpclog.Fatalf("failed starting http server: %v", err)
-    }
-  }
+	if *enableTls {
+		if err := httpServer.ListenAndServeTLS(*tlsCertFilePath, *tlsKeyFilePath); err != nil {
+			grpclog.Fatalf("failed starting http2 server: %v", err)
+		}
+	} else {
+		if err := httpServer.ListenAndServe(); err != nil {
+			grpclog.Fatalf("failed starting http server: %v", err)
+		}
+	}
 }
 
-type bookService struct{}
+type deviceService struct{}
 
-var books = []*library.Book{
-  {
-    Isbn:   60929871,
-    Rating: 4,
-    Title:  "Brave New World",
-    Author: "Aldous Huxley",
-  },
-  {
-    Isbn:   140009728,
-    Rating: 4,
-    Title:  "Nineteen Eighty-Four",
-    Author: "George Orwell",
-  },
-  {
-    Isbn:   9780140301694,
-    Rating: 4,
-    Title:  "Alice's Adventures in Wonderland",
-    Author: "Lewis Carroll",
-  },
-  {
-    Isbn:   140008381,
-    Rating: 3,
-    Title:  "Animal Farm",
-    Author: "George Orwell",
-  },
+var devices = []*library.Device{
+	{
+		Id:          60929871,
+		Name:        "Alice",
+		Type:        "ESP32 D1 Mini",
+		LastContact: "2 mins ago",
+		Battery:     "15%",
+		Version:     "5.2.1",
+		Status:      "offline",
+	},
+	{
+		Id:          140009728,
+		Name:        "Bob",
+		Type:        "ESP32 D1 Mini",
+		LastContact: "2 mins ago",
+		Battery:     "15%",
+		Version:     "5.2.1",
+		Status:      "offline",
+	},
+	{
+		Id:          9780140301694,
+		Name:        "Charlie",
+		Type:        "ESP32 D1 Mini",
+		LastContact: "2 mins ago",
+		Battery:     "15%",
+		Version:     "5.2.1",
+		Status:      "offline",
+	},
+	{
+		Id:          140008381,
+		Name:        "David",
+		Type:        "ESP32 D1 Mini",
+		LastContact: "2 mins ago",
+		Battery:     "15%",
+		Version:     "5.2.1",
+		Status:      "offline",
+	},
 }
 
-func (s *bookService) GetBook(ctx context.Context, bookQuery *library.GetBookRequest) (*library.Book, error) {
-  grpc.SendHeader(ctx, metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-unary"))
-  grpc.SetTrailer(ctx, metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-unary"))
+func (s *deviceService) GetDevice(ctx context.Context, deviceQuery *library.GetDeviceRequest) (*library.Device, error) {
+	grpc.SendHeader(ctx, metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-unary"))
+	grpc.SetTrailer(ctx, metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-unary"))
 
-  for _, book := range books {
-    if book.Isbn == bookQuery.Isbn {
-      return book, nil
-    }
-  }
+	for _, device := range devices {
+		if device.Id == deviceQuery.Id {
+			return device, nil
+		}
+	}
 
-  return nil, grpc.Errorf(codes.NotFound, "Book could not be found")
+	return nil, grpc.Errorf(codes.NotFound, "Device could not be found")
 }
 
-func (s *bookService) QueryBooks(bookQuery *library.QueryBooksRequest, stream library.BookService_QueryBooksServer) error {
-  stream.SendHeader(metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-stream"))
-  for _, book := range books {
-    if strings.HasPrefix(book.Author, bookQuery.AuthorPrefix) {
-      stream.Send(book)
-    }
-  }
-  stream.SetTrailer(metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-stream"))
-  return nil
+func (s *deviceService) QueryDevices(e *library.Empty, stream library.DeviceService_QueryDevicesServer) error {
+	stream.SendHeader(metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-stream"))
+	for _, device := range devices {
+		stream.Send(device)
+	}
+	stream.SetTrailer(metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-stream"))
+	return nil
 }
